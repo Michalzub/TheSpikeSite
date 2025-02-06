0371616eeaf8071baea157153b8bf281fa9e87c7
Michal Zúbek <misozet5@gmail.com>
2025-02-06 21:29:58 +0100

    removed unused alpine import

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,11 +1,5 @@
     import './bootstrap';
 
-    import Alpine from 'alpinejs';
-
-    window.Alpine = Alpine;
-
-    Alpine.start();
-
     /*--------------------------*/
     /*--COMMENT POST AJAX-------*/
     /*--------------------------*/
